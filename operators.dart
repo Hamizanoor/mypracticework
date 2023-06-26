d85d73e2af0b2c96249239387b7b93e5ddaa322b
Hamizanoor <hamizanoor991@gmail.com>
2023-06-27 02:51:53 +0500

    addmypracticework

---
+++ operators.dart
@@ -0,0 +1,45 @@
+void main() {
+  // Arithmatic + - / * % ~/
+  var a = 30;
+  var b = 20;
+  print(a + b);
+  print(a - b);
+  print(a % b);
+  print(a * b);
+  print(a / b);
+  print(a ~/ b);
+  // relational < > <= >= != ==
+  var c = 30;
+  var d = 20;
+  print(c < d);
+  print(c > d);
+  print(c <= d);
+  print(c >= d);
+  print(c != d);
+  print(c == d);
+  //  test operators is is!
+  var e = 10;
+  var f = 20;
+  print(e is! int);
+  // increment decrement ++ --
+  var z = 20;
+  z++;
+  print(z);
+  var y = 10;
+  ++y;
+  print(y);
+  var g = 20;
+  g--;
+  print(g);
+  var v = 10;
+  --v;
+  print(v);
+  // assignment =
+
+  var num = 30;
+  num += 30;
+  print(num);
+  var num1 = 30;
+  num1 -= 30;
+  print(num1);
+}
